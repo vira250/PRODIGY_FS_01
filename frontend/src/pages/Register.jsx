47759d31f4c2337695b46d2d12ca034d83068616
vira250 <jviraj666@gmail.com>
2025-06-01 15:26:27 +0530

    Design a Register form and  create a Login file

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 const Register =() =>{
     return(
-        <div>
-            <div>
+        <div className="flex justify-center items-center h-screen">
+            <div className="shadow-lg px-8 py-5 border rounded-sm w-96">
+                <h2 className="text-lg font-bold mb-4 text-9xl">Registraction</h2>
                 <form>
-                    <div>
-                        <label htmlFor="username">UserName</label>
-                        <input type='text' placeholder="Enter Username"></input>
+                    <div className="mb-4">
+                        <label htmlFor="username" className="block text-gray-700 mb-1">UserName</label>
+                        <input type='text' placeholder="Enter Username" className="w-full px-3 py-2 border rounded-sm"></input>
                     </div>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input type='email' placeholder="Enter Email"></input>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 mb-1">Email</label>
+                        <input type='email' placeholder="Enter Email" className="w-full px-3 py-2 border rounded-sm"></input>
                     </div>
-                    <div>
-                        <label htmlFor="password">UserName</label>
-                        <input type="password" placeholder="Enter Password"></input>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-gray-700 mb-1">Password</label>
+                        <input type="password" placeholder="Enter Password" className="w-full px-3 py-2 border rounded-sm"></input>
                     </div>
 
-                    <button type="submit"> Submit</button>
+                    <button className="w-full bg-blue-600 text-white py-2"> Submit</button>
                 </form>
-                <div>
-                    <p>Already have account?</p>
-                    <a href="Register">Login</a>
+                <div className="text-center">
+                    <span>Already have account?</span>
+                    <Link to='/login' className="text-blue-500">Login</Link>
                 </div>
             </div>
         </div>
